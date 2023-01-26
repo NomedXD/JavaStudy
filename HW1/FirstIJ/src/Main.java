fbf3dd387b4a85e90021ad2668b0a221bc73d753
NomedXD <coovshow1@gmail.com>
2023-01-26 16:33:36 +0300

    IDEA files

---
+++ HW1/FirstIJ/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date curr = new Date();
+        System.out.printf("Hello Ivan! Today is %s",curr);
+    }
+}
