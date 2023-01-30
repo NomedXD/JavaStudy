3622f95a89d110e80fbef87ca8786d7a4511c4c5
Matoshka Ivan <coovshow1@gmail.com>
2023-01-30 23:27:30 +0300

    Fix with main arguments

--- HW1/First.java
+++ HW1/First.java
@@ -3,6 +3,7 @@ public class First{
 	public static void main(String[] args)
 	{
 		Date curr = new Date();
-		System.out.printf("Hello Ivan! Today is %s",curr);
+		System.out.printf("Hello Ivan! Today is %s\n",curr);
+		System.out.printf("Hello %s! Today is %s",args[0],args[1]);
 	}
 }
--- HW1/FirstIJ/src/Main.java
+++ HW1/FirstIJ/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date curr = new Date();
-        System.out.printf("Hello Ivan! Today is %s",curr);
+        System.out.printf("Hello Ivan! Today is %s\n",curr);
+        System.out.printf("Hello %s! Today is %s",args[0],args[1]);
     }
 }
